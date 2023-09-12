23a2241e297d7d9f5d23028a105c1bbd77c18398
vinvas94 <vintoniv1111@gmail.com>
2023-09-12 21:59:34 +0200

    add js1,2

---
+++ js/01-gallery.js
@@ -0,0 +1,54 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+// console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+listEl.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+listEl.addEventListener("click", hadleClick);
+function createMarkup(element) {
+  return element
+
+    .map(
+      ({ preview, original, description }) => `
+      
+  <li class="gallery__item">
+   <a class="gallery-link" href="${original}">
+ <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    /></a>
+ </li>`
+    )
+    .join("");
+}
+
+function hadleClick(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    ` <img src="${event.target.dataset.source}"
+     alt="${event.target.alt}"/> `,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", closeModal);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", closeModal);
+      },
+    }
+  );
+
+  instance.show();
+  function closeModal(ev) {
+    if (ev.code !== "Escape") {
+      return;
+    }
+    instance.close();
+  }
+}
